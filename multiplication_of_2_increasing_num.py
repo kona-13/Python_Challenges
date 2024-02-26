3539eeae5a3df1de3e16306dcec70154171806aa
Steven <77511759+kona-13@users.noreply.github.com>
2024-02-26 13:11:12 +0000

    Added line 30

--- multiplication_of_2_increasing_num.py
+++ multiplication_of_2_increasing_num.py
@@ -1,9 +1,9 @@
 import time
 
-print("Please enter a decimal for x: ")
+print("Please enter a number for x: ")
 x = float(input(">> "))
 initial_x = x
-print("Please enter a decimal for y:")
+print("Please enter a numberfor y:")
 y = float(input(">> "))
 initial_y = y
 print("Please enter a value for which you wish to increase x by each time:")
@@ -21,8 +21,10 @@ while user_count >= count:
     y += addition_y
     print(x * y)
     count += 1
-    #time.sleep(0.1)
 
 end = time.time()
+
+
 print("Time taken: ", end - start, " seconds")
 print("\nEntered values: ", "\nx: ", initial_x, "\ny: ", initial_y, "\nx increase: ", addition_x, "\ny increase: ", addition_y, "\nNumber of calculations: ", user_count)
+time.sleep(5) # This line is here to stop the program from exiting straight away - it is currently set as 5 seconds.
